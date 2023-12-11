67c5b76b3a38ed19ffd18d45ca2c81af8ea9c9c1
Christy Babu <christybabu26@gmail.com>
2023-12-11 23:44:28 +0530

    Re iterated DB schema model removed sessions and added new fields

---
+++ prisma/migrations/20231211180550_initial_db_models_schema_migration/migration.sql
@@ -0,0 +1,266 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Modules" AS ENUM ('INCOMES', 'EXPENSES', 'INVESTMENTS', 'SUBSCRIPTIONS', 'DEBTS', 'LENDS', 'REPORTS');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "name_hash" TEXT,
+    "role" "Roles" NOT NULL DEFAULT 'USER',
+    "order_identifier" TEXT,
+    "order_store_id" TEXT,
+    "order_number" TEXT,
+    "order_status" TEXT,
+    "billing_start_date" TEXT,
+    "plan_status" TEXT NOT NULL DEFAULT 'basic',
+    "trial_start_date" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "usage" INTEGER NOT NULL DEFAULT 0,
+    "basic_usage_limit_email" BOOLEAN NOT NULL DEFAULT false,
+    "new_signup_email" BOOLEAN NOT NULL DEFAULT false,
+    "premium_plan_expired_email" BOOLEAN NOT NULL DEFAULT false,
+    "premium_usage_limit_email" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "feedbacks" (
+    "id" SERIAL NOT NULL,
+    "message" TEXT NOT NULL,
+    "user_id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "feedbacks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "notifiers" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "name_hash" TEXT,
+    "user_id" UUID NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "notify_date" TIMESTAMP(3) NOT NULL,
+    "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "repeat" BOOLEAN DEFAULT false,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notifiers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "name_hash" TEXT,
+    "currency" TEXT NOT NULL DEFAULT 'INR',
+    "locale" TEXT NOT NULL DEFAULT 'en',
+    "monthly_email_report" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "incomes" (
+    "id" TEXT NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "amount" TEXT NOT NULL DEFAULT '0',
+    "name_hash" TEXT,
+    "category_id" INTEGER,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "incomes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "expenses" (
+    "id" TEXT NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "amount" TEXT NOT NULL DEFAULT '0',
+    "name_hash" TEXT,
+    "paid_via" TEXT NOT NULL DEFAULT '',
+    "category_id" INTEGER,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "expenses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "debts" (
+    "id" TEXT NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "amount" TEXT NOT NULL DEFAULT '0',
+    "name_hash" TEXT,
+    "category_id" INTEGER,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "debts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "lends" (
+    "id" TEXT NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "amount" TEXT NOT NULL DEFAULT '0',
+    "name_hash" TEXT,
+    "category_id" INTEGER,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "lends_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "investments" (
+    "id" TEXT NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "amount" TEXT NOT NULL DEFAULT '0',
+    "units" TEXT NOT NULL DEFAULT '0',
+    "name_hash" TEXT,
+    "category_id" INTEGER,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "investments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subscriptions" (
+    "id" TEXT NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "amount" TEXT NOT NULL DEFAULT '0',
+    "paid" TEXT NOT NULL,
+    "url" TEXT,
+    "name_hash" TEXT,
+    "notify" BOOLEAN NOT NULL DEFAULT false,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3) NOT NULL,
+    "active" BOOLEAN DEFAULT true,
+    "cancelled_at" TEXT,
+    "category_id" INTEGER,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "subscriptions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reports" (
+    "id" TEXT NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "amount" TEXT NOT NULL DEFAULT '0',
+    "name_hash" TEXT,
+    "category_id" INTEGER,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "reports_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "profile_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "name_hash" TEXT,
+    "module_type" "Modules" NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "feedbacks" ADD CONSTRAINT "feedbacks_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notifiers" ADD CONSTRAINT "notifiers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notifiers" ADD CONSTRAINT "notifiers_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "incomes" ADD CONSTRAINT "incomes_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "incomes" ADD CONSTRAINT "incomes_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "expenses" ADD CONSTRAINT "expenses_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "expenses" ADD CONSTRAINT "expenses_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "debts" ADD CONSTRAINT "debts_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "debts" ADD CONSTRAINT "debts_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "lends" ADD CONSTRAINT "lends_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "lends" ADD CONSTRAINT "lends_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "investments" ADD CONSTRAINT "investments_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "investments" ADD CONSTRAINT "investments_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "categories" ADD CONSTRAINT "categories_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
